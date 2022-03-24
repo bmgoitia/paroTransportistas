74ea57dfe70dde45584186cf7e70da43629ae3ac
bmgoitia <65491589+bmgoitia@users.noreply.github.com>
2022-03-24 09:49:27 +0100

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+window.onload = function() {
+   
+  
+    $('.sect').on('click', function(e) {
+        // console.log(this.id)
+
+        let idValue = this.id
+        $('.' + idValue).show().siblings().hide();
+        $('.sect').removeClass( "mSel" );
+        $('#' + idValue).addClass('mSel');
+        
+
+
+    });
+
+ 
+};

95887ff6baa086c6cb003b826787bb910ddde81c
bmgoitia <65491589+bmgoitia@users.noreply.github.com>
2023-03-29 14:25:18 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ window.onload = function() {
    
   
     $('.sect').on('click', function(e) {
-        // console.log(this.id)
+        console.log(this.id)
 
         let idValue = this.id
         $('.' + idValue).show().siblings().hide();
